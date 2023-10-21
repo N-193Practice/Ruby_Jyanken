38fe0f0b0f97dce02e73aae37c46b44a2386e84d
N-193Practice <keep_me_nk0914@icloud.com>
2023-10-21 17:11:56 +0900

    Update jyanken.rb
    修正

--- jyanken.rb
+++ jyanken.rb
@@ -2,56 +2,60 @@
 ROCK = 'グー'
 PAPER = 'パー'
 SCISSORS = 'チョキ'
-NOFIGHT = '戦わない' 
+NOFIGHT = '戦わない'
 
-# プレーヤーが出す手を入力
-puts 'じゃんけんの手を選んでください (0: グー, 1: チョキ, 2: パー, 3: 戦わない):'
-player_choice = gets.chomp.to_i
+# じゃんけんの関数
+def play_janken
+  puts 'じゃんけんの手を選んでください (0: グー, 1: チョキ, 2: パー, 3: 戦わない):'
+  player_choice = gets.chomp.to_i
+  # 相手プレイヤーがランダムで手を選ぶ
+  computer_choice = [ROCK, PAPER, SCISSORS, NOFIGHT].sample
 
-# 相手プレーヤーがランダムで手を選ぶ
-computer_choice = [ROCK, PAPER, SCISSORS, NOFIGHT].sample
+  player_hand = [ROCK, SCISSORS, PAPER, NOFIGHT][player_choice]
+  puts "あなたの手: #{player_hand}"
+  puts "相手の手: #{computer_choice}"
 
-# じゃんけんの勝敗を判定
-if player_choice == 0
-  player_hand = ROCK
-elsif player_choice == 1
-  player_hand = SCISSORS
-elsif player_choice == 2
-  player_hand = PAPER
-elsif player_choice == 3
-  player_hand = NOFIGHT
-else
-  puts '無効な選択です。'
-  exit
+  if player_hand == computer_choice
+    puts '引き分けです.'
+    return play_janken
+  elsif player_hand == NOFIGHT || computer_choice == NOFIGHT
+    puts '戦わないを選びました。戦いを回避します.'
+    return play_janken
+  else
+    # プレイヤーがじゃんけんに勝った時
+    if (player_hand == ROCK && computer_choice == SCISSORS) ||
+       (player_hand == SCISSORS && computer_choice == PAPER) ||
+       (player_hand == PAPER && computer_choice == ROCK)
+      puts 'じゃんけんに勝利しました。あっち向いてホイの指を選択してください。'
+      your_choice = play_atchi_muite_hoi
+      if your_choice == computer_choice
+        puts 'あなたの勝利です。(じゃんけん終了)'
+      else
+        puts 'あなたはあっち向いてホイに失敗しました。(じゃんけんの最初に戻ります)'
+        return play_janken
+      end
+    else
+      puts '相手がじゃんけんに勝利しました。あっち向いてホイの顔の向きを選択してください。'
+      your_choice = play_atchi_muite_hoi
+      if your_choice == computer_choice
+        puts '相手の勝利です。(じゃんけん終了)'
+      else
+        puts '相手があっち向いてホイに失敗しました。(じゃんけんの最初に戻ります)'
+        return play_janken
+      end
+    end
+  end
 end
 
-puts "あなたの手: #{player_hand}"
-puts "相手の手: #{computer_choice}"
-
-if player_hand == computer_choice
-  puts '引き分けです.'
-elsif (player_hand == ROCK && computer_choice == SCISSORS) ||
-      (player_hand == SCISSORS && computer_choice == PAPER) ||
-      (player_hand == PAPER && computer_choice == ROCK)
-  puts 'あなたの勝ちです.'
+# あっち向いてホイの関数
+def play_atchi_muite_hoi
   puts 'あっち向いてホイ (0: 上, 1: 右, 2: 下, 3: 左):'
   your_choice = gets.chomp.to_i
   computer_choice = rand(4)
   puts "相手: #{computer_choice}"
-  if your_choice == computer_choice
-    puts 'あなたの勝利!'
-  else
-    puts '相手の勝利!'
-  end
-else
-  puts 'あなたの負けです.'
-  puts 'あっち向いてホイ (0: 上, 1: 右, 2: 下, 3: 左):'
-  your_choice = gets.chomp.to_i
-  computer_choice = rand(4)
-  puts "相手: #{computer_choice}"
-  if your_choice == computer_choice
-    puts '相手の勝利!'
-  else
-    puts 'あなたの勝利!'
-  end
+  return your_choice
 end
+
+# ゲーム開始
+play_janken
+
